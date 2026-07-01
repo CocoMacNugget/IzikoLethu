43738aba18bdcfb63d7652785f107cdbb3bda6f8
Safiya Elmi <safiyamelmi19@gmail.com>
2026-07-01 11:31:47 +0200

    Modify landlords table structure

--- IzikoLethu/data/landlords (1).sql
+++ IzikoLethu/data/landlords (1).sql
@@ -11,7 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -43,15 +42,8 @@ CREATE TABLE IF NOT EXISTS `landlords` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`landlord_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Landlord Information';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Landlord Information';
 
---
--- Dumping data for table `landlords`
---
-
-INSERT INTO `landlords` (`landlord_id`, `full_name`, `email`, `password`, `contact`, `business_name`, `number_of_properties`, `id_document`, `proof_of_ownership`, `preferred_student_type`, `lease_duration`, `created_at`) VALUES
-(1, 'Andy Brown', 'andybrown9@gmail.com', 'andybrown9', '0673456789', 'Andy\'s co.', 1, 'uploads/68ecf777cc7fd_Screenshot (50).png', 'uploads/68ecf777cc803_Screenshot 2025-02-11 164447.png', 'Any', 'Flexible', '2025-10-13 13:02:10'),
-(2, 'Larry Gray', 'larraygray78@gmail.com', 'larraygray7', '0457862378', 'Larry co.', 1, 'uploads/68ee3ef9402ab_Screenshot 2025-02-11 165501.png', 'uploads/68ee3ef940382_Screenshot 2025-02-11 165953.png', 'Any', '12 Months', '2025-10-14 12:16:01');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
